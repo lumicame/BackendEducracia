15bc757d8c8c66ccfaf2c62523ec62da8c75c1cc
Luis Mi Cabarcas <lumicame3@gmail.com>
2021-11-16 12:02:36 -0500

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,11 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Department;
+use App\Models\Type;
+
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +18,24 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+
+        $type=new Type();
+        $type->name="Educación";
+        $type->save();
+
+        $department=new Department();
+        $department->name="Armenia, Quindio";
+        $department->save();
+
+        $user=new User();
+        $user->name="Administrador";
+        $user->email="admin@educracia.com";
+        $user->password="admin2021";
+        $user->nit=0;
+        $user->type="admin";
+        $user->department_id=1;
+        $user->save();
         // \App\Models\User::factory(10)->create();
     }
 }
+
