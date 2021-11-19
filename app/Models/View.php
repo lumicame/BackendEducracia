509b56a2a33c02f8a492bd87721d81c5917a59fc
Luis Mi Cabarcas <lumicame3@gmail.com>
2021-11-19 16:09:18 -0500

    Update View.php

--- app/Models/View.php
+++ app/Models/View.php
@@ -8,10 +8,30 @@ use Illuminate\Database\Eloquent\Model;
 class View extends Model
 {
     use HasFactory;
+    protected $appends = ['created_at_formatted', 'updated_at_formatted'];
+    public function getCreatedAtFormattedAttribute()
+{
+    $this->user;
+    $this->history;
+    if ($this->created_at) {
+        return $this->created_at->diffForHumans();
+    } else {
+        return "";
+    }
+}
+
+public function getUpdatedAtFormattedAttribute()
+{
+    if ($this->updated_at) {
+        return $this->updated_at->diffForHumans();
+    } else {
+        return "";
+    }
+}
     public function user(){
         return $this->belongsTo(User::class);
     }
-    public function project(){
-        return $this->belongsTo(Project::class);
+    public function history(){
+        return $this->belongsTo(History::class);
     }
 }
