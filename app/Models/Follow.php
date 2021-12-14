5de6222b5e6f05a322c951037b9820670b535668
Luis Mi Cabarcas <lumicame3@gmail.com>
2021-12-14 01:04:52 -0500

    Update Follow.php

--- app/Models/Follow.php
+++ app/Models/Follow.php
@@ -8,6 +8,23 @@ use Illuminate\Database\Eloquent\Model;
 class Follow extends Model
 {
     use HasFactory;
+    protected $appends = ['created_at_formatted'];
+     
+    public function getCreatedAtFormattedAttribute()
+{
+    $this->user;
+    if ($this->created_at) {
+     // $day=\Carbon\Carbon::now()->diffInDays($this->created_at);
+      if ($this->created_at->isToday()) {
+         return $this->created_at->format('h:i A');
+      }else{
+         return $this->created_at->format('M d, h:i A');
+      }
+        
+    } else {
+        return "";
+    }
+}
     public function user(){
         return $this->belongsTo(User::class);
     }
